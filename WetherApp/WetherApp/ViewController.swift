3fe3dc559adabe99c6facce4f0ab87538b8dd386
Kiryll <supragak@gmail.com>
2021-02-22 22:19:40 +0300

    12

--- WetherApp/WetherApp/ViewController.swift
+++ WetherApp/WetherApp/ViewController.swift
@@ -5,26 +5,101 @@
 //  Created by Кирилл on 2/18/21.
 //
 import UIKit
+import Alamofire
+import CoreLocation
+import SwiftyJSON
+import NVActivityIndicatorView
 
-class ViewController: UIViewController, UISearchResultsUpdating {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var background: UIView!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var dyaLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    
+    let gradientLayer = CAGradientLayer()
+    let apiKey = "618d5a756ac5aa0b8f39201f28346146"
+    var lat = 11.344533
+    var lon = 104.33322
+    var activityIndicator: NVActivityIndicatorView!
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
+        background.layer.addSublayer(gradientLayer)
         
-        self.setupNavigationBar()
+        let indicatorSize: CGFloat = 70
+        let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
+        activityIndicator.backgroundColor = UIColor.black
+        view.addSubview(activityIndicator)
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        activityIndicator.startAnimating()
+        if(CLLocationManager.locationServicesEnabled()){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
     }
-    private func setupNavigationBar() {
-        self.navigationItem.title = "WeatherApp"
-        self.navigationController?.navigationBar.prefersLargeTitles = true
-        
-        let searchController = UISearchController(searchResultsController: nil)
-        searchController.searchResultsUpdater = self
-        
-        navigationItem.searchController = searchController
-        navigationItem.hidesSearchBarWhenScrolling = false
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setBlueGradientBackground()
     }
-    func updateSearchResults(for searchController: UISearchController){
-        print(searchController.searchBar.text)
-}
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        lat = location.coordinate.latitude
+        lon = location.coordinate.longitude
+        Alamofire.request("https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON {
+            response in
+            self.activityIndicator.stopAnimating()
+            if let responseStr = response.result.value {
+                let jsonResponse = JSON(responseStr)
+                let jsonWeather = jsonResponse["weather"].array![0]
+                let jsonTemp = jsonResponse["main"]
+                let iconName = jsonWeather["icon"].stringValue
+                self.cityLabel.text = jsonResponse["name"].stringValue
+                self.weatherImage.image = UIImage(named: iconName)
+                self.weatherDescriptionLabel.text = jsonWeather["main"].stringValue
+                self.tempLabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))"
+                let date = Date()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "EEEE"
+                self.dyaLabel.text = dateFormatter.string(from: date)
+                
+                let suffix = iconName.suffix(1)
+                if(suffix == "n"){
+                    self.setGrayGradientColor()
+                }else{
+                    self.setBlueGradientBackground()
+                }
+            }
+        }
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    
+  
+    func setBlueGradientBackground(){
+        let topColor = UIColor(red: 95.0/255.0, green: 165.0/255.0, blue: 1.0, alpha: 1.0).cgColor
+        let bottomColor = UIColor(red: 72.0/255.0, green: 114.0/255.0, blue: 184.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [topColor, bottomColor]
+    }
+    
+    func setGrayGradientColor(){
+        let topColor = UIColor(red: 151.0/255.0, green: 151.0/255.0, blue: 151.0/255.0, alpha: 1.0).cgColor
+        let bottomColor = UIColor(red: 72.0/255.0, green: 72.0/255.0, blue: 72.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [topColor, bottomColor]
+    }
     
 }
